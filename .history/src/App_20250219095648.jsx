2d832bf98dd63f2270bcc34a2afa530c006f247b
Mohammadreza Zarif <m.zariffard1381@gmail.com>
2025-02-27 09:04:56 +0200

    added everything untill header

---
+++ .history/src/App_20250219095648.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect ,useState } from "react";
+import Search from "./components/Search.jsx";
+
+const API_BASE_URL= 'https://api.themoviedb.org/3/discover/movie';
+
+const API_KEY=import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS={
+  method : 'GET',
+  headers: {
+    accept: 'application/jason',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
+
+const App = () => {
+  const[searchTerm,setSearchTerm]=useState('');
+
+  const fetchMovies = async ()=>{
+    try{
+
+    }catch(error){
+      console.error(`Error Fetching Movies: ${Error}`);
+    }
+  }
+
+  useEffect(()=>{
+
+  },[]);
+  
+
+  return (
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero-img.png" alt="Hero Img" />
+          <h1>
+            {" "}
+            Find <span className="text-gradient">Movie</span> You'll Enjoy
+            Without The Hastle
+          </h1>
+        </header>
+      </div>
+
+      <Search  searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+    </main>
+  );
+};
+
+export default App;
---
+++ .history/src/App_20250219101039.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect ,useState } from "react";
+import Search from "./components/Search.jsx";
+
+const API_BASE_URL= 'https://api.themoviedb.org/3/discover/movie';
+
+const API_KEY=import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS={
+  method : 'GET',
+  headers: {
+    accept: 'application/jason',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
+
+const App = () => {
+  const[searchTerm,setSearchTerm]=useState('');
+
+  const[errorMessage , setErrorMessage]=useState(null);
+
+  const fetchMovies = async ()=>{
+    try{
+      const endpoint=`${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      
+
+      const response=await fetch(endpoint , API_OPTIONS);
+
+      if(!response.ok){
+        throw new Error('Failed to fetch movies');
+      }
+
+      const data = await response.jason();
+    }
+    catch(error){
+      console.error(`Error Fetching Movies: ${error}`);
+      setErrorMessage(`Error Fetching Movies: Try Again Later.`);
+    }
+  }
+
+  useEffect(()=>{
+
+  },[]);
+  
+
+  return (
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero-img.png" alt="Hero Img" />
+          <h1>
+            {" "}
+            Find <span className="text-gradient">Movie</span> You'll Enjoy
+            Without The Hastle
+          </h1>
+
+          <Search  searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+      </div>
+
+      <section className="all-movies">
+        <h2> All Movies </h2>
+
+        {errorMessage && <p className="text-red-500"> {errorMessage}</p>}
+      </section>
+
+      
+    </main>
+  );
+};
+
+export default App;
