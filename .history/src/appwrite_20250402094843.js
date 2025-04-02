fe7093d8bdef9134201b88675019fc3e4dde10d3
Mohammadreza Zarif <m.zariffard1381@gmail.com>
2025-04-02 09:59:47 +0300

    fix(database): I fixed a bug for the searchTerms with the database

--- .history/src/appwrite_20250402094843.js
+++ .history/src/appwrite_20250402094843.js
@@ -4,11 +4,6 @@ const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
 
-console.log("Project ID:", PROJECT_ID);
-console.log("Database ID:", DATABASE_ID);
-console.log("Collection ID:", COLLECTION_ID);
-
-
 const client = new Client()
   .setEndpoint('https://cloud.appwrite.io/v1')  // Corrected endpoint
   .setProject(PROJECT_ID);
